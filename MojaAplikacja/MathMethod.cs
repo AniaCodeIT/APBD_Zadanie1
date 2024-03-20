d999e505ebe92f396635c87b25a798a8b171e3ed
s25630 <s25630@pjwstk.edu.pl>
2024-03-20 23:32:34 +0100

    FindMax function implemented

--- MojaAplikacja/MathMethod.cs
+++ MojaAplikacja/MathMethod.cs
@@ -11,4 +11,15 @@ public class MathMethod {
 
         return (double)sum / numbers.Length;
     }
+
+    public static int FindMax(int[] numbers) {
+        int max = numbers[0];
+        foreach(int num in numbers) {
+            if(num > max) {
+                max = num;
+            }
+        }
+
+        return max;
+    }
 }
